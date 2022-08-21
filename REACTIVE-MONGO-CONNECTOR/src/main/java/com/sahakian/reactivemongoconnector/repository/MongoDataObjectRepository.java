c1e65fdd4f13982d3d6cefb599950d2c2515d5ea
sahak1an <sahakyan.i1997@gmail.com>
2022-08-21 20:30:52 +0400

    init structure

--- REACTIVE-MONGO-CONNECTOR/src/main/java/com/sahakian/reactivemongoconnector/repository/MongoDataObjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sahakian.reactivemongoconnector.repository;
-
-import com.sahakian.reactivemongoconnector.data.MongoDataObject;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MongoDataObjectRepository extends ReactiveMongoRepository<MongoDataObject,String> {
-}
